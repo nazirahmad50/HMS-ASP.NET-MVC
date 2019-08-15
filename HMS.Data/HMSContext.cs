aee4a4d24a0ac9386e7fbdf88692fd2fa6903136
Nazir <n_ahmad20@live.co.uk>
2019-08-15 18:22:08 +0100

    Setup DBcontext & Entiy Framework / enable migrations

---
+++ HMS.Data/HMSContext.cs
@@ -0,0 +1,24 @@
+﻿using HMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Data
+{
+    public class HMSContext : DbContext
+    {
+        public HMSContext() : base("HMSConnectionString")
+        {
+
+        }
+
+        public DbSet<AccomadationType> AccomadationType { get; set; }
+        public DbSet<AccomadationPackage> AccomadationPackage { get; set; }
+        public DbSet<Accomadation> Accomadation { get; set; }
+        public DbSet<Booking> Booking { get; set; }
+
+    }
+}

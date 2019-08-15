f60e1c2a7f078e9d73ad32497a5aadb71b4e60b9
Nazir <n_ahmad20@live.co.uk>
2019-08-15 18:01:21 +0100

    Add Initial entities

---
+++ HMS.Entities/Accomadation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    /// <summary>
+    /// Aprtment No. or Hotel Room No. based on Accomadation Package
+    /// </summary>
+    public class Accomadation
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int AccomadationPackageID { get; set; }
+        public AccomadationPackage AccomadationPackage { get; set; } // AccomadationPackage object
+    }
+}
---
+++ HMS.Entities/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        public DateTime FromDate { get; set; }
+        /// <summary>
+        /// No of stays nights
+        /// </summary>
+        public int Duration { get; set; }
+
+        public int AccomadationID { get; set; }
+        public Accomadation Accomadation { get; set; } // Accomadation object
+    }
+}

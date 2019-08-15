ea636fc2f723095661fa6fa4c742a424e10f0d78
Nazir <n_ahmad20@live.co.uk>
2019-08-15 19:03:59 +0100

    Create User entity / add identityFramework to Data & Entities / Move identityDBContext to Data / add migration 'IdentityFramework'

--- HMS.Data/HMSContext.cs
+++ HMS.Data/HMSContext.cs
@@ -1,4 +1,5 @@
 ﻿using HMS.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,17 +9,26 @@ using System.Threading.Tasks;
 
 namespace HMS.Data
 {
-    public class HMSContext : DbContext
+    // inherit from 'IdentityDbContext' instead of 'DBContext' because we want to use Users as well
+    // Through Identity Framework Microsoft will create all the necessary tables
+    public class HMSContext : IdentityDbContext<HMSUser>
     {
         public HMSContext() : base("HMSConnectionString")
         {
 
         }
 
+        public static HMSContext Create()
+        {
+            return new HMSContext();
+        }
+
+        // Tables in database
         public DbSet<AccomadationType> AccomadationType { get; set; }
         public DbSet<AccomadationPackage> AccomadationPackage { get; set; }
         public DbSet<Accomadation> Accomadation { get; set; }
         public DbSet<Booking> Booking { get; set; }
+
 
     }
 }
